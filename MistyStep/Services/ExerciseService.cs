231ab587b14848f00a8696851225dc28fb5b53e6
Abdul <ara@skyhost.dk>
2025-03-08 21:50:29 +0100

    refactor

--- MistyStep/Services/ExerciseService.cs
+++ MistyStep/Services/ExerciseService.cs
@@ -9,18 +9,53 @@ public class ExerciseService : IExerciseService
         var temp = exercise;
         if (temp is not null)
         {
+            ExerciseTestList.ExerciseList.Add(exercise);
             return true;
         }
         return false;
     }
-    
+
     public async Task<bool> CreateNewExerciseProgram(ExerciseProgram program)
     {
         var temp = program;
         if (temp is not null)
         {
+            ExerciseTestList.ExerciseProgramList.Add(program);
             return true;
         }
         return false;
     }
+}
+
+public static class ExerciseTestList
+{
+    public static List<Exercise> ExerciseList { get; set; } = [
+        new Exercise("Pushups"),
+        new Exercise("Situps"),
+        new Exercise("Pullups"),
+        new Exercise("Chinups"),
+        new Exercise("Lunges"),
+        new Exercise("Punches")
+    ];
+
+    public static List<ExerciseProgram> ExerciseProgramList { get; set; } = [
+        new ExerciseProgram("Program 1", [
+            new("Pushups"),
+            new("Situps"),
+            new("Pullups"),
+            new("Chinups"),
+            new("Lunges"),
+            new("Punches")
+        ]),
+        new ExerciseProgram("Program 2",[
+            new("Pushups"),
+            new("Situps"),
+            new("Pullups"),
+            new("Punches")
+        ]),
+        new ExerciseProgram("Program 3",[
+            new("Lunges"),
+            new("Punches")
+        ]),
+        ];
 }
