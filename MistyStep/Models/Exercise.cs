a2077c3bc6475d692bf4b8d1c6b98db2a190c562
Abdul <ara@skyhost.dk>
2025-03-31 00:21:40 +0200

    feat: Create exercise dialog

--- MistyStep/Models/Exercise.cs
+++ MistyStep/Models/Exercise.cs
@@ -1,6 +1,30 @@
 namespace MistyStep.Models;
 
-public record Exercise(Guid Id, string Name, TimeSpan ExerciseDuration);
+public class Exercise
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public TimeSpan? ExerciseDuration { get; set; }
+
+    public Exercise(string Name, TimeSpan ExerciseDuration)
+    {
+        this.Id = Guid.NewGuid();
+        this.Name = Name;
+        this.ExerciseDuration = ExerciseDuration;
+    }
+
+    public Exercise(Guid Id, string Name, TimeSpan ExerciseDuration)
+    {
+        this.Id = Id;
+        this.Name = Name;
+        this.ExerciseDuration = ExerciseDuration;
+    }
+
+    public Exercise()
+    {
+        this.Id = Guid.NewGuid();
+    }
+}
 
 public record ExerciseProgram(Guid Id, string Name, List<Exercise> Exercises, TimeSpan PauseDuration);
 
